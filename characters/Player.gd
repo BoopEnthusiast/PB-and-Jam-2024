ea6ca4ec9f7c41950a1c524272edf02f71c1043e
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-29 23:51:12 +1200

    paper interaction

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,26 +1,51 @@
 class_name Player extends CharacterBody2D
 
 
+# Constants
 const SPEED = 400.0
 const ACCEL = SPEED * 10
 const DECEL = ACCEL * 1.3
 
+# Get nodes
 @onready var interact_area = $InteractArea
+@onready var phone = $Popup/Phone
+@onready var phone_text = $Popup/Phone/PhoneText
+@onready var paper = $Popup/Paper
+@onready var paper_text = $Popup/Paper/PaperText
 
 var interacting := false
 
 
 func _physics_process(delta):
+	# Interaction
 	if Input.is_action_just_pressed("interact"):
-		for area in interact_area.get_overlapping_areas():
-			if area is Paper:
-				interacting = true
-				break
+		var just_switched = false
+		if interacting:
+			stop_interacting()
+			just_switched = true
+		if not interacting and not just_switched:
+			for area in interact_area.get_overlapping_areas():
+				if area is Paper:
+					interacting = true
+					paper_text.text = area.text
+					paper.visible = true
+					break
 	
+	elif Input.is_action_just_pressed("ui_cancel") and interacting:
+		stop_interacting()
+	
+	# Movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	if direction:
+	
+	if direction and not interacting:
 		velocity = velocity.move_toward(direction * SPEED, ACCEL * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, DECEL * delta)
 	
 	move_and_slide()
+
+
+func stop_interacting() -> void:
+	interacting = false
+	phone.visible = false
+	paper.visible = false
--- level/interactables/paper.gd
+++ level/interactables/paper.gd
@@ -1,3 +1,4 @@
 class_name Paper extends Area2D
 
 
+@export_multiline var text: String
