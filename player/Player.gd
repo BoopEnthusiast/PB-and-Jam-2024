14812f7465cc29e129ae8492b53c4a0140ef80ee
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-29 15:41:35 +1200

    change movement

--- player/Player.gd
+++ player/Player.gd
@@ -2,15 +2,15 @@ class_name Player extends CharacterBody2D
 
 
 const SPEED = 400.0
-const ACCEL = SPEED / 3.0
-const DECEL = ACCEL / 2.0
+const ACCEL = SPEED * 10
+const DECEL = ACCEL * 1.3
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity = velocity.move_toward(direction * SPEED, ACCEL)
+		velocity = velocity.move_toward(direction * SPEED, ACCEL * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, DECEL)
+		velocity = velocity.move_toward(Vector2.ZERO, DECEL * delta)
 	
 	move_and_slide()
