4de3e7f657f255dd49f38188b74f6c2fd7a394c8
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-29 23:11:15 +1200

    paper stuff

--- characters/Player.gd
+++ characters/Player.gd
@@ -5,8 +5,18 @@ const SPEED = 400.0
 const ACCEL = SPEED * 10
 const DECEL = ACCEL * 1.3
 
+@onready var interact_area = $InteractArea
+
+var interacting := false
+
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("interact"):
+		for area in interact_area.get_overlapping_areas():
+			if area is Paper:
+				interacting = true
+				break
+	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, ACCEL * delta)
