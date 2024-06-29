5bde27dbefaac4370e30e93d2a4116c3db228c89
TakuikaNinja <takuikaninja@gmail.com>
2024-06-29 12:58:42 +1200

    Create player character

---
+++ player/PlayerCharacter.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+var SPEED = 400
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+	
+	move_and_slide()
